b6d4684e819d4641bbbb027908d02c49adf56be6
Dmitry Toforov <toforovdima@yandex.ru>
2023-04-10 14:06:13 +0300

    some editing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
-import Body from "../../components/Body";
-import Footer from "../../components/Footer";
-import Header from "../../components/Header";
+import Body from '../../components/Body'
+import Footer from '../../components/Footer'
+import Header from '../../components/Header'
 
 export default function Home() {
   return (
@@ -11,4 +11,3 @@ export default function Home() {
     </>
   )
 }
- 
