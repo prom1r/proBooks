5e1332d973e32aa2274a63b87d82a47f30782045
Dmitry Toforov <toforovdima@yandex.ru>
2023-04-11 12:59:52 +0300

    added new fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,18 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#1a202c',
+        'secondary': '#718096',
+        'active': '#38b2ac',
+      },
+    },
   },
   plugins: [],
 }
